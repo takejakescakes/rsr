c6031a675acc64472bdc13f1c23e0fd5a436efaf
Jason Thompson <takejakescake@gmail.com>
2020-03-15 16:06:03 -0400

    upload works

--- blog_example/__init__.py
+++ blog_example/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e1487f9bb26793dcdf3a8350295aa8c1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bc = Bcrypt(app)
+log = LoginManager(app)
+log.login_view = 'login'
+log.login_message_category = 'info'
 
 from blog_example import routes
--- blog_example/routes.py
+++ blog_example/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template, url_for, flash, redirect
-from blog_example import app
-from blog_example.forms import RegistrationForm, LoginForm
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request
+from blog_example import app, db, bc
+from blog_example.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from blog_example.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 post = [
 	{
@@ -29,19 +33,70 @@ def about():
 
 @app. route('/register', methods=['GET', 'POST'])
 def register():
+	if current_user.is_authenticated:
+		flash(f'You\'re already logged in!', 'danger')
+		return redirect(url_for('home'))
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		flash(f'Account created for {form.username.data}.', 'success')
-		return redirect(url_for('home'))
+		hsh = bc.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data, email=form.email.data, password=hsh)
+		db.session.add(user)
+		db.session.commit()
+		flash(f'New account created.', 'success')
+		return redirect(url_for('login'))
 	return render_template('register.html', title="Register", form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+		flash(f'You\'re already logged in!', 'danger')
+		return redirect(url_for('home'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.email.data == 'fake@email.com' and form.password.data == '12':
-			flash('You are now logged in.', 'success')
-			return redirect(url_for('home'))
+		u = User.query.filter_by(email=form.email.data).first()
+		print(u)
+		if u and bc.check_password_hash(u.password, form.password.data):
+			login_user(u, remember=form.remember.data)
+			next_page = request.args.get('next')
+			return redirect(next_page) if next_page else redirect(url_for('home'))
 		else:
 			flash('You don\'t belong here.', 'danger')
 	return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
+
+def save_picture(form_picture):
+	random_hex = secrets.token_hex(8)
+	_, f_ext = os.path.splitext(form_picture.filename)
+	picture_fn = random_hex + f_ext
+	picture_path = os.path.join(app.root_path, 'static/img', picture_fn)
+	output_size = (125, 125)
+	i = Image.open(form_picture)
+	i.thumbnail(output_size)
+	i.save(picture_path)
+	return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+	form = UpdateAccountForm()
+	print(current_user.image_file)
+	if form.validate_on_submit():
+		f = form.picture.data
+		if f:
+			picture_file = save_picture(f)
+			current_user.image_file = picture_file
+		current_user.username = form.username.data
+		current_user.email = form.email.data
+		db.session.commit()
+		flash('Changes saved.', 'success')
+		return redirect(url_for('account'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
+		form.picture.data = current_user.image_file
+	image_file = url_for('static', filename='img/' + current_user.image_file)
+	return render_template('account.html', title='Account', image_file=image_file, form=form)
