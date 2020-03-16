91cfec5856eccff32f84874971573b81e4787710
Jason Thompson <takejakescake@gmail.com>
2020-03-15 20:45:24 -0400

    reset password

--- blog_example/__init__.py
+++ blog_example/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e1487f9bb26793dcdf3a8350295aa8c1'
@@ -11,5 +13,11 @@ bc = Bcrypt(app)
 log = LoginManager(app)
 log.login_view = 'login'
 log.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'takejakescake'
+app.config['MAIL_PASSWORD'] = 'zffhpcpaxoadfzyy'
+mail = Mail(app)
 
 from blog_example import routes
